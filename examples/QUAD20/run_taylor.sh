d6f31bad374836489ce5061ca19d5b0be0f55ab6
Jiameng <jmfan@bu.edu>
2022-06-02 14:46:03 -0400

    update 12-dimension quadrotor example

---
+++ examples/QUAD20/run_taylor.sh
@@ -0,0 +1,24 @@
+make &&\
+    # ./quad20 0.05 50 4 2 0 Taylor &&\
+    # ./quad20 0.05 50 4 3 0 Taylor &&\
+    # ./quad20 0.05 50 4 4 0 Taylor &&\
+    # ./quad20 0.05 50 4 2 1 Taylor &&\
+
+    ./quad20 0.1 50 4 2 0 Taylor &&\
+    ./quad20 0.1 50 4 3 0 Taylor &&\
+    ./quad20 0.1 50 4 4 0 Taylor &&\
+    ./quad20 0.1 50 4 2 1 Taylor &&\
+
+    ./quad20 0.2 50 4 2 0 Taylor &&\
+    ./quad20 0.2 50 4 3 0 Taylor &&\
+    ./quad20 0.2 50 4 4 0 Taylor &&\
+    ./quad20 0.2 50 4 2 1 Taylor &&\
+    ./quad20 0.2 50 4 3 1 Taylor &&\
+    ./quad20 0.2 50 4 4 1 Taylor &&\
+
+    ./quad20 0.4 50 4 2 0 Taylor &&\
+    ./quad20 0.4 50 4 3 0 Taylor &&\
+    ./quad20 0.4 50 4 4 0 Taylor &&\
+    ./quad20 0.4 50 4 2 1 Taylor &&\
+    ./quad20 0.4 50 4 3 1 Taylor &&\
+    ./quad20 0.4 50 4 4 1 Taylor
