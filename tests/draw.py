fee713a11103171e9119c570b5aae239a74be346
zwc662 <zwc662@gmail.com>
2022-12-21 23:58:52 -0500

    Update ReLU plot drawer

--- tests/draw.py
+++ tests/draw.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+plt.rcParams['text.usetex'] = True
 xys = []
 f = open("bp_relu_plt.txt", "r")
 line = f.readline()
@@ -12,16 +12,16 @@ f.close()
 
 xys = np.asarray(xys)
  
-plt.title(label = "The interval remainder for the TM is [-BP(0)/2, BP(0)/2]")
+plt.title(label = r"The interval remainder for the TM is $[-\frac{p_{B,k}(0)}{2}, \frac{p_{B,k}(0)}{2}]$")
 xs = xys[:, 0]
 ys1 = xys[:, 1]
 ys2 = xys[:, 2]
 ys3 = xys[:, 3]
 ys4 = xys[:, 4]
 xys = None
-plt.plot(xs, ys1, label = "ReLU(x)")
+plt.plot(xs, ys1, label = r"$ReLU(x)$")
 plt.plot(xs, ys2, linestyle='dashed')
-plt.plot(xs, ys3, label = "BP(x)-BP(0)/2")
+plt.plot(xs, ys3, label = r"$p_{B,k}(x)-\frac{p_{B,k}(0)}{2}$")
 plt.plot(xs, ys4, linestyle='dashed')
 
 plt.xlabel("x")
